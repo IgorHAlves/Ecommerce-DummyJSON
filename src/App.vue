c14cdd65776fc04ef163a197fd54ed6128bed87c
Igor Alves <igor.h.alves.ads@gmail.com>
2025-05-29 22:56:30 -0300

    Iniciada navbar

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/NavbarComponent.vue'
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="bg-black text-white flex justify-between mt-5 h-15">
+        <!-- Logo -->
+        <h1 class="flex ml-10 items-center hover:underline hover:cursor-pointer">Ecommerce</h1>
+
+        <!-- Barra de pesquisa -->
+        <div class="flex items-center">
+            <input type="text" class=" w-120 bg-white text-black h-10 rounded pl-2 focus:outline-none"
+                placeholder="Pesquise o produto">
+        </div>
+        <!-- Navegação -->
+        <ul class="flex gap-15 mr-10 items-center">
+            <li class="hover:underline hover:cursor-pointer">Produtos</li>
+            <li class="hover:underline hover:cursor-pointer">Categorias</li>
+            <li class="hover:underline hover:cursor-pointer">Carrinho</li>
+        </ul>
+    </nav>
+</template>
