88c716866c190995462912d81c928777ff21b8ec
unknown <r4f4santoss@gmail.com>
2025-05-30 23:41:42 -0300

    consumo api Dummy tela Home

--- src/components/CardProdutoComponent.vue
+++ src/components/CardProdutoComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="text-white">
-        <img class="h-40" src="products.images" alt="products.title">
+    <div class="text-black gap-4 ">
+        <img class="h-40" :src="products.images[0]" :alt="products.title">
         <h1 class="text-left font-bold">{{ products.title }}</h1>
         <p class="text-left">{{products.price}}</p>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="min-h-screen">
     <h1>Teste</h1>
-    <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-2">
+    <div class="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
         <CardProduto
           v-for="product in products"
           :key="product.title"
@@ -12,18 +12,27 @@
 </template>
 
 
-<script>
+<script setup>
   import axios from 'axios'
+  import { ref, onMounted } from "vue"
   import CardProduto from '../components/CardProdutoComponent.vue'
-  import{ref, onMounted} from "vue"
 
   const products = ref([])
-  const offset = ref (0)
+  const skip = ref (0)
   const limit = 20
 
-  const fetch = async () => {
-    const response = await axios.get(`'https://dummyjson.com/products?offset${offset.value}&limit${limit.value}'`)
-    const results = response.data.products
-    console.log(products.value);  
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(`https://dummyjson.com/products?skip=${skip.value}&limit=${limit}`)
+      const results = response.data.products;
+      products.value = results
+
+      console.log(products.value);
+
+    } catch (error) {
+      console.error("Erro ao buscar produtos:", error);
+    }
   } 
+
+  onMounted(fetchProduct);
 </script>
