a9acb6527e4db2cebe3f73511eb1b441893fb38b
auauauau <auauauau@gmail.com>
2025-06-04 20:06:25 -0300

    arrumou tamanho imagem

--- src/components/CardProdutoComponent.vue
+++ src/components/CardProdutoComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="`/product/${products.id}`" class="block">
-    <div class="bg-gray-50 border-2 border-black p-2 text-black gap-4 hover:shadow-lg cursor-pointer">
+    <div class="bg-gray-50 border-2 border-black p-2 text-black hover:shadow-lg cursor-pointer w-92">
       <img class="flex justify-center w-full " :src="products.images[0]" :alt="products.title" />
       <h1 class="text-left font-bold mt-2">{{ products.title }}</h1>
       <p class="text-left">R$ {{ products.price }}</p>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,25 @@
 <template>
   <main class="min-h-screen flex flex-col justify-center p-4">
-    <!--Banner-->
-    <div class="w-full h-[400px] bg-black ">
-
-    </div>
-    <!--Produtos-->
-    <div>
-      <h1 class="text-center items-center font-bold py-10 text-4xl ">Produtos</h1>
-    </div>
-    <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 ">
-      <CardProduto  v-for="product in products" :key="product.title" :products="product" />
+   <div class=" flex  flex-col items-center">
+     <!--Banner-->
+      <div class="w-full h-[400px] bg-black ">
+        <img src="" alt="">
+      </div>
+      <!--Produtos-->
+      <div>
+        <h1 class="text-center items-center font-bold py-10 text-4xl ">Produtos</h1>
+      </div>
+      <div class="grid  gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 ">
+        <CardProduto  v-for="product in products" :key="product.title" :products="product" />
+      </div>
+      
     </div>
     <!--Botão-->
-  <div class="flex justify-end items-center gap-4 p-4 mt-5">
-      <button class="bg-black text-white rounded-lg w-10 hover:cursor-pointer" @click="prevPage"><</button>
-      <button @click="nextPage" class="bg-black rounded-lg text-white w-10 hover:cursor-pointer">></button>
-    </div>
+    <div class="flex justify-end items-center gap-4 p-4 mt-5">
+        <button class="bg-black text-white rounded-lg w-10 hover:cursor-pointer" @click="prevPage"><</button>
+        <button @click="nextPage" class="bg-black rounded-lg text-white w-10 hover:cursor-pointer">></button>
+      </div>
+   
   </main>
 
 </template>
