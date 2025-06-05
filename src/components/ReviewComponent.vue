608f644cf9746f8b31272dd971eacb7fbeed4842
auauauau <auauauau@gmail.com>
2025-06-04 21:37:38 -0300

    criação da descrição

---
+++ src/components/ReviewComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="shadow-lg">
+      <div class="bg-beige-100 p-2 text-black  w-92">
+        <h1 class="text-left font-bold mt-2">Nota: {{ review.rating }}</h1>
+        <h1 class="text-left font-bold mt-2">Comment: {{ review.comment }}</h1>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    review: Object
+  })
+  </script>
--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -15,9 +15,16 @@
         <h1 class="font-bold text-lg">Descrição:</h1>
         <p class="mt-2">{{ product.description }}</p>
         <p class="text-lg mt-4 font-bold">R$ {{ product.price }}</p>
-        <button class="bg-black text-white p-4 w-50 mt-10">Adicionar ao carrinho</button>
+        <p class="text-lg mt-4 font-bold">R$ {{ product.dimensions.width }}</p>
+        <button class="bg-black text-white p-4 w-50 mt-10">Adicionar ao carrinho</button>     
       </div>
     </div>
+    <div class="mt-20"> 
+        <ReviewComponent 
+        v-for="review in product.reviews"
+         :key="review.reviewName" 
+         :review="review" 
+         /></div>
     </div>
     
  
@@ -28,17 +35,27 @@
   import axios from 'axios'
   import {useRoute} from 'vue-router'
   import {ref, onMounted} from 'vue'
+  import ReviewComponent from '../components/ReviewComponent.vue'
 
   const route = useRoute()
   const product = ref({})
 
-  const fetchProduct = async() => {
+  
+  const fetchProductDetail = async() => {
     const id = route.params.id
     const response = await axios.get(`https://dummyjson.com/products/${id}`)
     const results = response.data
     product.value = results
+
+    const dimensions = await Promise.all(
+        product.then(response => response.data))
+
+    console.log(dimensions.width);
+    
   }
-  onMounted(fetchProduct)
+
+
+  onMounted(fetchProductDetail)
 
 </script>
 
